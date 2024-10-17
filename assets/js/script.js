a2af7d7cabb7bbea4e09a87eb9dd52e9cf87a1a3
Gowri Sankar Koushik Pasumarthy <gowrisankarkoushik@gmail.com>
2024-10-17 14:21:59 -0400

    portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -239,3 +239,4 @@ srtop.reveal('.experience .timeline .container', { interval: 400 });
 /* SCROLL CONTACT */
 srtop.reveal('.contact .container', { delay: 400 });
 srtop.reveal('.contact .container .form-group', { delay: 400 });
+
